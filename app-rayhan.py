3f76c83fcf1b505ba01f1200d2ff5fee07642434
Rayhan1k24 <daynarayhan@gmail.com>
2025-05-18 22:19:22 +0200

    modification du message flask

--- app-rayhan.py
+++ app-rayhan.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps test sur!"
+    return "Hello, DevOps tp reussis!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
